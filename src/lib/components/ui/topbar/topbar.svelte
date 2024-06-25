cdc34b8e1c30ced1e61600c1574855b7eab310a0
Rid <alridho2003@gmail.com>
2024-06-25 17:43:10 +0800

    Updated topbar

--- src/lib/components/ui/topbar/topbar.svelte
+++ src/lib/components/ui/topbar/topbar.svelte
@@ -3,33 +3,80 @@
     import * as Command from "$lib/components/ui/command";
     import * as Popover from "$lib/components/ui/popover";
     import { Button } from "$lib/components/ui/button";
+    import * as Avatar from "$lib/components/ui/avatar";
+    import { cn } from "$lib/utils.js";
+    import { tick } from "svelte";
 
-    const sensordId = [
+    const sensorsId = [
         {
-            value: "sensor1",
+            value: "sensor 1",
             label: "Sensor 1",
         },
         {
-            value: "sensor2",
+            value: "sensor 2",
             label: "Sensor 2",
         },
         {
-            value: "sensor3",
+            value: "sensor 3",
             label: "Sensor 3",
         },
     ];
+
+    $: selectedValue =
+        sensorsId.find((f) => f.value === value)?.label ?? "Select a sensor...";
+
+    function closeAndFocusTrigger(triggerId: string) {
+        open = false;
+        tick().then(() => {
+            document.getElementById(triggerId)?.focus();
+        });
+    }
 
     let open = false;
     let value = "";
 </script>
 
-<Popover.Root bind:open>
-    <Popover.Trigger>
-        <Button variant="outline">
-            Select a sensor...
-            <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-    </Popover.Trigger>
-    <Popover.Content>Place content for the popover here.</Popover.Content>
-</Popover.Root>
+<nav class="flex justify-between">
+    <Popover.Root bind:open let:ids>
+        <Popover.Trigger>
+            <Button variant="outline" class="w-[200px] justify-between">
+                {selectedValue}
+                <ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+        </Popover.Trigger>
+        <Popover.Content class="w-[200px] p-0">
+            <Command.Root>
+                <Command.Input placeholder="Search sensor ID..." />
+                <Command.Empty>No sensor found.</Command.Empty>
+                <Command.Group>
+                    {#each sensorsId as sensor}
+                        <Command.Item
+                            value={sensor.value}
+                            onSelect={(currentValue) => {
+                                value = currentValue;
+                                closeAndFocusTrigger(ids.trigger);
+                            }}
+                        >
+                            <Check
+                                class={cn(
+                                    "mr-2 h-4 w-4",
+                                    value !== sensor.value &&
+                                        "text-transparent",
+                                )}
+                            />
+                            {sensor.label}
+                        </Command.Item>
+                    {/each}
+                </Command.Group>
+            </Command.Root>
+        </Popover.Content>
+    </Popover.Root>
 
+    <Avatar.Root>
+        <Avatar.Image
+            src="https://avatars.githubusercontent.com/u/113193641?s=400&u=4fe489f1a74e40830e7f8f21e7450252fd469ead&v=4"
+            alt="rid333"
+        />
+        <Avatar.Fallback>RD</Avatar.Fallback>
+    </Avatar.Root>
+</nav>
